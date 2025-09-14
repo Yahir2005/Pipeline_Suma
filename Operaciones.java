dfa1bc872aa740f018f1cd247f43655994a2035a
Yahir <Luisyajairo@outlook.com>
2025-09-13 20:42:07 -0600

    prueba suma

--- Operaciones.java
+++ Operaciones.java
@@ -1,17 +1,17 @@
-import java.util.*;
-
 public class Operaciones {
     public static void main(String[]args){
-        Scanner read = new Scanner(System.in);
-        int n1,n2;
-        System.out.println("Ingrese su numero 1");
-        n1 = read.nextInt();
+        int n1 = 0,n2=0;
 
-        System.out.println("Ingrese su numero 2");
-        n2 = read.nextInt();
-
-        System.out.print(sumar(n1,n2));
-
+        try {
+        System.out.println("Ingrese el Número 1");
+        n1 = Integer.parseInt(args[0]);
+        System.out.println("Ingrese el Número 2");
+        n2 = Integer.parseInt(args[1]);
+        System.out.print("La suma es: "+sumar(n1,n2));
+        } catch (Exception e) {
+            System.out.println("Error en tiempo de ejecución: "+e.getMessage());
+        }
+        
     }
     public static int sumar(int n1, int n2){
         
